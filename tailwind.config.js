62a3acacaa9f6d07a2720d1d89498308c36b7562
Adnan Mursyid <adnanmursyid@gmail.com>
2024-10-11 20:54:31 +0700

    v1.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}',
-    './src/**/*.{js,jsx,ts,tsx}'
+  content: ['./public/**/*.{html,js}'
   ],
   safelist: [
     {
